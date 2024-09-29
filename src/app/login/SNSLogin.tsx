5c38896540e1c20d768d91a8c6848534abc3bfa2
gaeunnlee <gaeunnnleee@gmail.com>
2024-09-30 00:47:31 +0900

    feat: oAuth 리다이렉션 링크 수정

--- src/app/login/SNSLogin.tsx
+++ src/app/login/SNSLogin.tsx
@@ -2,27 +2,21 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import { Google, Kakao, Naver } from '@/assets/images/SNS/SNS-Image';
 import { styles } from './styles.css';
 
 export default function SNSLogin() {
-  const route = useRouter();
-
   return (
     <div className={styles.buttonContainer}>
       <Link href={process.env.NEXT_PUBLIC_KAKAO_OAUTH!}>
         <Image src={Kakao.src} alt="SNS Login Logo" width={45} height={45} />
       </Link>
-      <button
-        type="button"
-        onClick={() => {
-          route.push(process.env.NEXT_PUBLIC_NAVER_OAUTH!);
-        }}
-      >
+      <Link href={process.env.NEXT_PUBLIC_NAVER_OAUTH!}>
         <Image src={Naver.src} alt="SNS Login Logo" width={45} height={45} />
-      </button>
-      <Image src={Google.src} alt="SNS Login Logo" width={45} height={45} />
+      </Link>
+      <Link href={process.env.NEXT_PUBLIC_GOOGLE_OAUTH!}>
+        <Image src={Google.src} alt="SNS Login Logo" width={45} height={45} />
+      </Link>
     </div>
   );
 }
