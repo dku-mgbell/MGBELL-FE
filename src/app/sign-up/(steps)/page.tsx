f76de1d60cb64e7d260d06fcfbe054160b1eebde
gaeunnlee <gaeunnnleee@gmail.com>
2024-09-30 01:17:33 +0900

    fix: wrap useSearchParams 컴포넌트 Suspense 처리

--- src/app/sign-up/(steps)/page.tsx
+++ src/app/sign-up/(steps)/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, Suspense, useEffect, useState } from 'react';
 import { UserRole } from '@/types/user';
 import { OwnerImage, UserImage } from '@/assets/images/sign-up/image';
 import { useRouter, useSearchParams } from 'next/navigation';
@@ -24,58 +24,60 @@ export default function Page() {
   }, []);
 
   return (
-    <SignUpLayout
-      title="어떤 목적으로 마감벨을 사용하시나요?"
-      isNextStepAllowed={!!userRole}
-      onNextStep={() => {
-        route.push('/sign-up/email');
-        setSignUpInfo({ ...signUpInfo, userRole });
-      }}
-    >
-      <div className={userStyles.container}>
-        <label className={userStyles.button}>
-          <input
-            type="radio"
-            name="user-type"
-            value="USER"
-            onChange={handleButtonClick}
-          />
-          <Image
-            className={userStyles.image}
-            alt="user icon"
-            src={UserImage.src}
-            width={78}
-            height={92}
-          />
-          <strong>일반 사용자</strong>
-          <p>
-            상품을 저렴하게
-            <br />
-            구입하고 싶어요!
-          </p>
-        </label>
-        <label className={userStyles.button}>
-          <input
-            type="radio"
-            name="user-type"
-            value="OWNER"
-            onChange={handleButtonClick}
-          />
-          <Image
-            className={userStyles.image}
-            alt="user icon"
-            src={OwnerImage.src}
-            width={92}
-            height={93}
-          />
-          <strong>자영업자</strong>
-          <p>
-            남은 음식을
-            <br />
-            판매하고 싶어요!
-          </p>
-        </label>
-      </div>
-    </SignUpLayout>
+    <Suspense>
+      <SignUpLayout
+        title="어떤 목적으로 마감벨을 사용하시나요?"
+        isNextStepAllowed={!!userRole}
+        onNextStep={() => {
+          route.push('/sign-up/email');
+          setSignUpInfo({ ...signUpInfo, userRole });
+        }}
+      >
+        <div className={userStyles.container}>
+          <label className={userStyles.button}>
+            <input
+              type="radio"
+              name="user-type"
+              value="USER"
+              onChange={handleButtonClick}
+            />
+            <Image
+              className={userStyles.image}
+              alt="user icon"
+              src={UserImage.src}
+              width={78}
+              height={92}
+            />
+            <strong>일반 사용자</strong>
+            <p>
+              상품을 저렴하게
+              <br />
+              구입하고 싶어요!
+            </p>
+          </label>
+          <label className={userStyles.button}>
+            <input
+              type="radio"
+              name="user-type"
+              value="OWNER"
+              onChange={handleButtonClick}
+            />
+            <Image
+              className={userStyles.image}
+              alt="user icon"
+              src={OwnerImage.src}
+              width={92}
+              height={93}
+            />
+            <strong>자영업자</strong>
+            <p>
+              남은 음식을
+              <br />
+              판매하고 싶어요!
+            </p>
+          </label>
+        </div>
+      </SignUpLayout>
+    </Suspense>
   );
 }
