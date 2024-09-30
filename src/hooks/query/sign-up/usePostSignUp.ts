2355f3478ae08833f27f6c5cb43800284aae7d24
gaeunnlee <gaeunnnleee@gmail.com>
2024-09-30 12:16:55 +0900

    feat: OWNER 유저 회원가입 후 라우팅 변경

--- src/hooks/query/sign-up/usePostSignUp.ts
+++ src/hooks/query/sign-up/usePostSignUp.ts
@@ -1,14 +1,19 @@
 import { User } from '@/hooks/api/user';
+import { useSignUpInfoStore } from '@/hooks/stores/useSignUpInfoStore';
 import { SignUpInfo } from '@/types/sign-up';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 
 export const usePostSignUp = () => {
   const route = useRouter();
+  const {
+    signUpInfo: { userRole },
+  } = useSignUpInfoStore();
+
   return useMutation({
     mutationFn: (data: SignUpInfo) => User.signUp(data),
     onSuccess: () => {
-      route.push('/sign-up/success');
+      route.push(userRole === 'USER' ? '/sign-up/success' : '/register/store');
     },
   });
 };
