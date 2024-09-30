828bcb4638cb8a7505322976dd1b63116d604e68
gaeunnlee <gaeunnnleee@gmail.com>
2024-09-30 11:44:53 +0900

    fix: 회원가입 페이지 컴포넌트 분리 후 suspense로 wrap

--- src/app/sign-up/(steps)/page.tsx
+++ src/app/sign-up/(steps)/page.tsx
@@ -9,7 +9,7 @@ import { useSignUpInfoStore } from '@/hooks/stores/useSignUpInfoStore';
 import SignUpLayout from '../(layout)/SignUpLayout';
 import { userStyles } from './styles.css';
 
-export default function Page() {
+function SignUpPageContent() {
   const [userRole, setUserRole] = useState<UserRole | null>(null);
   const { signUpInfo, setSignUpInfo } = useSignUpInfoStore();
   const route = useRouter();
@@ -24,60 +24,66 @@ export default function Page() {
   }, []);
 
   return (
-    <Suspense>
-      <SignUpLayout
-        title="어떤 목적으로 마감벨을 사용하시나요?"
-        isNextStepAllowed={!!userRole}
-        onNextStep={() => {
-          route.push('/sign-up/email');
-          setSignUpInfo({ ...signUpInfo, userRole });
-        }}
-      >
-        <div className={userStyles.container}>
-          <label className={userStyles.button}>
-            <input
-              type="radio"
-              name="user-type"
-              value="USER"
-              onChange={handleButtonClick}
-            />
-            <Image
-              className={userStyles.image}
-              alt="user icon"
-              src={UserImage.src}
-              width={78}
-              height={92}
-            />
-            <strong>일반 사용자</strong>
-            <p>
-              상품을 저렴하게
-              <br />
-              구입하고 싶어요!
-            </p>
-          </label>
-          <label className={userStyles.button}>
-            <input
-              type="radio"
-              name="user-type"
-              value="OWNER"
-              onChange={handleButtonClick}
-            />
-            <Image
-              className={userStyles.image}
-              alt="user icon"
-              src={OwnerImage.src}
-              width={92}
-              height={93}
-            />
-            <strong>자영업자</strong>
-            <p>
-              남은 음식을
-              <br />
-              판매하고 싶어요!
-            </p>
-          </label>
-        </div>
-      </SignUpLayout>
+    <SignUpLayout
+      title="어떤 목적으로 마감벨을 사용하시나요?"
+      isNextStepAllowed={!!userRole}
+      onNextStep={() => {
+        route.push('/sign-up/email');
+        setSignUpInfo({ ...signUpInfo, userRole });
+      }}
+    >
+      <div className={userStyles.container}>
+        <label className={userStyles.button}>
+          <input
+            type="radio"
+            name="user-type"
+            value="USER"
+            onChange={handleButtonClick}
+          />
+          <Image
+            className={userStyles.image}
+            alt="user icon"
+            src={UserImage.src}
+            width={78}
+            height={92}
+          />
+          <strong>일반 사용자</strong>
+          <p>
+            상품을 저렴하게
+            <br />
+            구입하고 싶어요!
+          </p>
+        </label>
+        <label className={userStyles.button}>
+          <input
+            type="radio"
+            name="user-type"
+            value="OWNER"
+            onChange={handleButtonClick}
+          />
+          <Image
+            className={userStyles.image}
+            alt="user icon"
+            src={OwnerImage.src}
+            width={92}
+            height={93}
+          />
+          <strong>자영업자</strong>
+          <p>
+            남은 음식을
+            <br />
+            판매하고 싶어요!
+          </p>
+        </label>
+      </div>
+    </SignUpLayout>
+  );
+}
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignUpPageContent />
     </Suspense>
   );
 }
