08fcd366e40966869f972b9cf93f8df47c3037f4
gaeunnlee <gaeunnnleee@gmail.com>
2024-10-01 19:03:15 +0900

    feat: 매장 등록 페이지 레이아웃 작성

---
+++ src/components/question-container/question-container.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import { styles } from './styles.css';
+
+export default function QuestionContainer({
+  title,
+  desc,
+  content,
+}: {
+  title: string;
+  desc?: string | ReactNode;
+  content: ReactNode;
+}) {
+  return (
+    <div className={styles.container}>
+      <strong className={styles.title}>{title}</strong>
+      {desc && <p className={styles.desc}>{desc}</p>}
+      {content}
+    </div>
+  );
+}
---
+++ src/components/question-container/styles.css.ts
@@ -0,0 +1,24 @@
+import { colors } from '@/styles/constant';
+import { globalStyle, style } from '@vanilla-extract/css';
+
+export const styles = {
+  container: style({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 5,
+  }),
+  title: style({
+    fontSize: 18,
+  }),
+  desc: style({
+    fontSize: 15,
+    color: colors.lightGray150,
+    marginBottom: 6,
+    lineHeight: 1.4,
+  }),
+};
+
+globalStyle(`${styles.desc} span`, {
+  fontSize: 15,
+  lineHeight: 1.4,
+});
