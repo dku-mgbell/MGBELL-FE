d7a03ab29ca16f7520945536ee92ee30b1c89cfd
gaeunnlee <gaeunnnleee@gmail.com>
2024-10-19 04:04:44 +0900

    feat: 가게 DTO 변경

--- src/types/store.ts
+++ src/types/store.ts
@@ -1,11 +1,9 @@
 import { Coordinate } from './map';
 
-export type StoreID = 'FOOD' | 'BAKERY' | 'CAFE' | 'DESERT' | 'ETC';
+export type StoreID = 'BAKERY' | 'DESERT' | 'ETC';
 
 export const StoreName = {
-  FOOD: '음식점',
   BAKERY: '베이커리',
-  CAFE: '카페',
   DESSERT: '디저트',
   ETC: '기타',
 } as const;
