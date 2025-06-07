a4694ec6f275e492f2f4e481c5c591af9f4d621d
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-08 05:24:16 +0900

    feat: 매장 등록 초기 페이지 작성 (#24)

--- src/app/register/store/info/layout.tsx
+++ src/app/register/store/info/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import HeaderLayout from '@/components/layout/header-layout/header-layout';
+import HeaderLayout from '@/components/layout/header-layout';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return <HeaderLayout title="매장 등록">{children}</HeaderLayout>;
