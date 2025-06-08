ae28a1845e0a672ebd36ce70a0b7b7b415a69812
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-08 16:06:05 +0900

    feat: 주소 검색 훅 작성, 매장 주소 입력 필드 작성 (#24)

--- src/app/register/store/info/page.tsx
+++ src/app/register/store/info/page.tsx
@@ -6,11 +6,14 @@ import FormLayout from '@/components/layout/form-layout';
 import LabeledField from '@/components/ui/labeled-field';
 import TextField from '@/components/ui/text-field';
 import { zodResolver } from '@hookform/resolvers/zod';
+import useSearchAddress from '@/hooks/useSearchAddress';
 
 type StoreForm = z.infer<typeof schema>;
 
 const schema = z.object({
   storeName: z.string().min(1, { message: '' }),
+  address: z.string().min(1, { message: '' }),
+  detailAddress: z.string().optional(),
 });
 
 export default function Page() {
@@ -18,13 +21,20 @@ export default function Page() {
     register,
     handleSubmit,
     formState: { errors },
+    setValue,
   } = useForm<StoreForm>({
     resolver: zodResolver(schema),
     mode: 'onChange',
   });
+  const { address, openAddressModal, coordData } = useSearchAddress({
+    setValue,
+  });
 
   const onSubmit: SubmitHandler<StoreForm> = (data) => {
-    return data;
+    return {
+      ...data,
+      coord: [coordData?.addresses[0].x, coordData?.addresses[0].y],
+    };
   };
 
   return (
@@ -37,6 +47,23 @@ export default function Page() {
           placeholder="매장 이름 입력"
           register={register}
           name="storeName"
+          errors={errors}
+        />
+      </LabeledField>
+      <LabeledField label="매장 주소">
+        <TextField
+          name="address"
+          placeholder="매장 주소 입력"
+          register={register}
+          errors={errors}
+          value={address}
+          onClick={openAddressModal}
+          onKeyDown={openAddressModal}
+        />
+        <TextField
+          name="detailAddress"
+          placeholder="상세 주소 입력 (선택)"
+          register={register}
           errors={errors}
         />
       </LabeledField>
--- src/components/layout/form-layout/index.tsx
+++ src/components/layout/form-layout/index.tsx
@@ -9,7 +9,10 @@ export default function FormLayout({
   onSubmit: FormEventHandler<HTMLFormElement>;
 }) {
   return (
-    <form onSubmit={onSubmit} className="flex flex-col gap-[20px] pb-[90px]">
+    <form
+      onSubmit={onSubmit}
+      className="flex flex-col gap-[20px] py-[20px] pb-[90px]"
+    >
       {children}
       <div className="bg-white fixed bottom-0 pb-[20px] w-[calc(100%-40px)] left-1/2 -translate-x-1/2 max-w-[410px]">
         <Button type="submit">등록</Button>
