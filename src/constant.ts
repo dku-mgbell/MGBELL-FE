a511468a1a31a49631111fd65c5fbeadfd0aa64f
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-19 02:45:14 +0900

    chore: OAuth Redirect Base Url 환경 변수 추가

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,4 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 export const WIP_API_BASE_URL = process.env.NEXT_PUBLIC_WIP_API_URL;
+export const OAUTH_REDIRECT_BASE_URL =
+  process.env.NEXT_PUBLIC_OAUTH_REDIRECT_BASE_URL;
