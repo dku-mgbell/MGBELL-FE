949abb86ec277077679bd789cdf6a3a903b471ee
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-21 04:24:27 +0900

    design: 현위치 버튼 위치 변경 (#46)

--- src/app/map/_components/location-button.tsx
+++ src/app/map/_components/location-button.tsx
@@ -3,7 +3,7 @@ import IconButton from '@/components/button/icon-button';
 
 export default function LocationButton({ onClick }: { onClick: () => void }) {
   return (
-    <div className="absolute z-[99] bottom-[100px] left-[25px]">
+    <div className="absolute z-[99] right-[25px] top-[calc(12px+env(safe-area-inset-top))]">
       <IconButton icon={<LocationIcon />} onClick={onClick} />
     </div>
   );
