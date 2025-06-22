e9e9c3cb53d2b071c87566d0c0c63556220f240c
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-23 02:57:19 +0900

    feat: cn 함수 custom class group 명시

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
 import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+const customTwMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [
+        'text-h1',
+        'text-h2',
+        'text-h3',
+        'text-h4',
+        'text-h5',
+        'text-b1',
+        'text-b2',
+        'text-b3',
+      ],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return customTwMerge(clsx(inputs));
 }
