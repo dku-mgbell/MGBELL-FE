3591fdac9647d20ddfc506db75747d4f69b29435
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-23 17:00:28 +0900

    feat: 매장 로그인 리다이렉트 비교값 수정

--- src/types/user.ts
+++ src/types/user.ts
@@ -37,6 +37,6 @@ export interface AccountInfo {
     userRole: 'CUSTOMER' | 'OWNER';
     providerType: keyof typeof loginButtonConfig;
     approved: 'APPROVED' | 'WAITING' | 'REJECTED';
-    goodsId: number | null;
+    goodsId: number | 'null';
   };
 }
