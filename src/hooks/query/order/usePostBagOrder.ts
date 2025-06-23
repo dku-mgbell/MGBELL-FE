1799da82d813992d0335f5455aa8f33709838041
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-24 01:22:46 +0900

    feat: 유저 주문 등록 로직 작성 (#53)

--- src/hooks/query/order/usePostBagOrder.ts
+++ src/hooks/query/order/usePostBagOrder.ts
@@ -1,25 +1,36 @@
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useUserPaymentStore } from '@/app/bag/order/_stores/useUserPaymentStore';
 import { Order } from '@/hooks/api/order';
 import useLoadingModal from '@/hooks/useModal/loading';
 import { ErrorResponse } from '@/types/api';
-import { OrderRequest } from '@/types/order';
+import { UserOrderRequest } from '@/types/order';
 import useModal from '@/hooks/useModal';
 
-export const usePostBagOrder = (price: number) => {
+export const usePostBagOrder = () => {
   const queryClient = useQueryClient();
   const route = useRouter();
   const { open } = useModal();
   const { openLoading, closeLoading } = useLoadingModal();
+  const { userPaymentStore, setUserPaymentStore } = useUserPaymentStore();
+
+  useEffect(() => {
+    if (userPaymentStore.merchantUid) {
+      route.push(`/bag/order/pay?merchantUid=${userPaymentStore.merchantUid}`);
+    }
+  }, [userPaymentStore]);
 
   return useMutation({
-    mutationFn: (data: OrderRequest) => Order.register(data),
-    onSuccess: async () => {
-      closeLoading();
-      await queryClient.invalidateQueries({
-        queryKey: ['user-order-list'],
+    mutationFn: (data: UserOrderRequest) => Order.register(data),
+    onSuccess: (res) => {
+      queryClient.invalidateQueries({ queryKey: ['user-order-list'] });
+      setUserPaymentStore({
+        ...userPaymentStore,
+        merchantUid: res.merchantUid,
+        amount: res.totalAmount,
       });
-      route.push(`/bag/order/success?price=${price}`);
+      closeLoading();
     },
     onMutate: () => {
       openLoading();
