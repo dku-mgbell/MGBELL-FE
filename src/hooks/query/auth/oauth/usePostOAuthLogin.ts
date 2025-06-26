f5436b2df71c818be5dc943ee9d6c3168158af45
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-26 17:25:22 +0900

    feat: OAuth 로그인 토큰 저장 로직 변경 (#28)

--- src/hooks/query/auth/oauth/usePostOAuthLogin.ts
+++ src/hooks/query/auth/oauth/usePostOAuthLogin.ts
@@ -4,6 +4,7 @@ import { useMutation } from '@tanstack/react-query';
 import { User } from '@/hooks/api/user';
 import { ErrorResponse } from '@/types/api';
 import { OAuthLoginRequest, SignUpData } from '@/types/sign-up';
+import { useAuth } from '@/hooks/useAuth';
 import useModal from '@/hooks/useModal';
 import { useGetUserAccountInfo } from '../../user/useGetUserAccountInfo';
 
@@ -14,6 +15,7 @@ export const usePostOAuthLogin = (nextPage?: string) => {
   const [isTokenIssued, setIsTokenIssued] = useState(false);
   const { mutate: getAccountInfo } = useGetUserAccountInfo({ redirect: true });
   const { open } = useModal();
+  const { setTokenResponse } = useAuth();
 
   useEffect(() => {
     // 토큰 발급시 정보 조회하여 userRole에 따라 리다이렉트
@@ -25,21 +27,40 @@ export const usePostOAuthLogin = (nextPage?: string) => {
   return useMutation({
     mutationFn: (data: SignUpData | OAuthLoginRequest) => User.oAuthLogin(data),
     onSuccess: (res) => {
-      const authHeader = res.headers.authorization;
-      const accessToken = authHeader ? authHeader.split(' ')[1] : undefined;
+      const {
+        authorization: accessTokenResponse,
+        refreshtoken: refreshTokenResponse,
+      } = res.headers;
+
+      if (!accessTokenResponse || !refreshTokenResponse) {
+        open({
+          content: '토큰 발급 오류',
+          confirmEvent: () => {
+            router.push('/login');
+          },
+          onlyConfirmButton: true,
+        });
+        return;
+      }
+
+      const [accessToken, refreshToken] = [
+        accessTokenResponse.split(' ')[1],
+        refreshTokenResponse.split(' ')[1],
+      ];
 
       // 토큰 저장
-      if (accessToken) {
-        localStorage.setItem('accessToken', accessToken);
-      }
-      // 회원가입 시 정보 입력
+      setTokenResponse({ accessToken, refreshToken });
+
+      // 신규 회원가입
       if (nextPage) {
+        // 명시한 회원가입 리다이렉 페이지로 이동
         router.push(nextPage);
       }
-
-      // 로그인
-      // 토큰 발급 여부 업데이트를 통하여 정보 조회 로직 실행
-      setIsTokenIssued(true);
+      // 기존 회원 로그인
+      else {
+        // 토큰 발급 여부 업데이트하여 정보 조회 로직 실행
+        setIsTokenIssued(true);
+      }
     },
     onError: (err: ErrorResponse<LoginErrorCode>) => {
       const errorCode = err.response.data.code;
