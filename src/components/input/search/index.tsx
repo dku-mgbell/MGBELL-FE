cabbea5336344bbfe0a907d8f42ffeb08553c791
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-28 03:06:00 +0900

    feat: SearchInput onEnter, input 기본 props 추가 (#47)

--- src/components/input/search/index.tsx
+++ src/components/input/search/index.tsx
@@ -1,17 +1,24 @@
+'use client';
+
 import { ForwardedRef, forwardRef } from 'react';
 import SearchIcon from '@/assets/svg/SearchIcon';
 import { cn } from '@/lib/utils';
 
+type SearchInputProps = React.ComponentProps<'input'> & {
+  onEnter?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+};
+
 export default forwardRef(function SearchInput(
-  {
-    placeholder,
-    className,
-  }: {
-    placeholder: string;
-    className?: string;
-  },
+  { className, onEnter, ...props }: SearchInputProps,
   ref: ForwardedRef<HTMLInputElement | null>,
 ) {
+  const handleEnterKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.nativeEvent.isComposing) return; // 한글 입력 시 조합 중인 상태에서는 이벤트 처리를 중단하여 이중 입력 방지
+    if (e.key === 'Enter') {
+      onEnter?.(e);
+    }
+  };
+
   return (
     <div
       className={cn(
@@ -23,7 +30,8 @@ export default forwardRef(function SearchInput(
       <input
         ref={ref}
         className="w-full bg-transparent text-[14px] font-medium text-gray4 placeholder:text-gray4"
-        placeholder={placeholder}
+        onKeyDown={handleEnterKeyDown}
+        {...props}
       />
     </div>
   );
