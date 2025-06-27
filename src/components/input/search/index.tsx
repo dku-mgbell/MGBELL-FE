2a00d44f2ff5c6122a1eaab6e34d2676b451f134
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-28 01:13:00 +0900

    feat: SearchInput 컴포넌트 forwardRef 래핑 (#47)

--- src/components/input/search/index.tsx
+++ src/components/input/search/index.tsx
@@ -1,15 +1,30 @@
-'use client';
-
+import { ForwardedRef, forwardRef } from 'react';
 import SearchIcon from '@/assets/svg/SearchIcon';
+import { cn } from '@/lib/utils';
 
-export default function SearchInput({ placeholder }: { placeholder: string }) {
+export default forwardRef(function SearchInput(
+  {
+    placeholder,
+    className,
+  }: {
+    placeholder: string;
+    className?: string;
+  },
+  ref: ForwardedRef<HTMLInputElement | null>,
+) {
   return (
-    <div className="flex items-center flex-1 gap-[9px] py-[10px] px-[15px] rounded-[10px] h-fit bg-white">
+    <div
+      className={cn(
+        'flex items-center flex-1 gap-[9px] py-[10px] px-[15px] rounded-[10px] h-fit bg-white',
+        className,
+      )}
+    >
       <SearchIcon />
       <input
+        ref={ref}
         className="w-full bg-transparent text-[14px] font-medium text-gray4 placeholder:text-gray4"
         placeholder={placeholder}
       />
     </div>
   );
-}
+});
