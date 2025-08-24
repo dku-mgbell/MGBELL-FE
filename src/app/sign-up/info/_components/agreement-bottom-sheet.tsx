da97d68198bb4a25f7bee738050e538fd6eaeaf1
gaeunnlee <gaeunnnleee@gmail.com>
2025-08-25 08:19:20 +0900

    feat: 공급자 회원용 이용약관 추가

--- src/app/sign-up/info/_components/agreement-bottom-sheet.tsx
+++ src/app/sign-up/info/_components/agreement-bottom-sheet.tsx
@@ -57,10 +57,18 @@ export default function AgreementBottomSheet({
             moreContentUrl="https://cake-innocent-6a8.notion.site/225493b7c9c6802abd7dd2613ca239ea?source=copy_link"
           />
           <AgreementItem
-            label="(필수) 이용약관(일반회원용)"
+            label={`(필수) 이용약관(${
+              signUpInfo.userRole === 'CUSTOMER'
+                ? '일반회원용'
+                : '공급자 회원용'
+            })`}
             isChecked={checkedState.terms}
             onChange={() => handleAgreementItemChange('terms')}
-            moreContentUrl="https://cake-innocent-6a8.notion.site/225493b7c9c68097b3b0ff5c4e7a8068?source=copy_link"
+            moreContentUrl={
+              signUpInfo.userRole === 'CUSTOMER'
+                ? 'https://cake-innocent-6a8.notion.site/225493b7c9c68097b3b0ff5c4e7a8068?source=copy_link'
+                : 'https://cake-innocent-6a8.notion.site/225493b7c9c6803bbea2f29412c137a4?source=copy_link'
+            }
           />
         </div>
         <Button onClick={handleSignUpButtonClick} disabled={!isAllChecked}>
